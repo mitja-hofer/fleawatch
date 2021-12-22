bb47acfb65ba1619e6d9f28874d1916370ffeb69
mitja-hofer <mitja.hofer@celtra.com>
2021-12-22 15:14:26 +0100

    functional ad parsing

--- fleawatch.py
+++ fleawatch.py
@@ -1,9 +1,22 @@
 from selenium import webdriver
+import selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
-
+class bolhaAd(object):
+    def __init__(self, content):
+        self.title = content.find_element_by_class_name("ClassifiedDetailSummary-title").text
+        self.id = content.find_element_by_class_name("ClassifiedDetailSummary-adCode").text.split(" ")[-1]
+        self.price = content.find_element_by_class_name("ClassifiedDetailSummary-priceDomestic").text
+        self.description = content.find_element_by_class_name("ClassifiedDetailDescription-text").text
+        self.type = content.find_elements_by_css_selector("dd.ClassifiedDetailBasicDetails-listDefinition")[0].text
+        self.location =content.find_elements_by_css_selector("dd.ClassifiedDetailBasicDetails-listDefinition")[1].text
+        self.condition = content.find_elements_by_css_selector("dd.ClassifiedDetailBasicDetails-listDefinition")[2].text
+        
+    def __str__(self):
+        return f"{self.title}\nŠifra oglasa:\t{self.id}\nCena:\t{self.price}\nLokacija:\t{self.location}\n\n"
+    
 browser = webdriver.Firefox()
 try:
     keywords = "cestno kolo"
@@ -13,9 +26,20 @@ try:
     el.click()
     el.send_keys(keywords + Keys.RETURN)
 
-    ads = WebDriverWait(browser, 20).until(lambda d: d.find_elements_by_css_selector(".EntityList--Regular .EntityList-item--Regular"))
-    for ad in ads:
-        print(ad.text)
+    # wait for async loading of ads
+    ad_links = WebDriverWait(browser, 20).until(
+        lambda d: d.find_elements_by_css_selector(".EntityList--Regular .EntityList-item--Regular h3 > a.link")
+    )
+    ad_hrefs = [ad_link.get_attribute("href") for ad_link in ad_links]
+    for ad_href in ad_hrefs:
+        browser.get(ad_href)
+        try: #new ad view
+            content = browser.find_element_by_class_name("content-main")
+            a = bolhaAd(content)
+            print(a)
+        except selenium.common.exceptions.NoSuchElementException:
+            browser.find_element_by_class_name("base-entity")
+            print("old ad")
 
 finally:
     browser.quit()
